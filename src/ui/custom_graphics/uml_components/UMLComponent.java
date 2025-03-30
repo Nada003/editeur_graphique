607cb4919e291f3e29147942e84f3fa8a64aeeb9
Nada <ng263921@gmail.com>
2025-03-30 22:25:10 +0100

    add: -name, attributes and functions in the popup

--- src/ui/custom_graphics/uml_components/UMLComponent.java
+++ src/ui/custom_graphics/uml_components/UMLComponent.java
@@ -30,8 +30,8 @@ public abstract class UMLComponent extends JPanel implements MouseListener, Drag
     protected UMLComponent() {
         setCursor(new Cursor(Cursor.HAND_CURSOR)); // Changes to hand cursor
 
-        positionX += count * 20;
-        positionY += count * 20;
+        positionX = 400 + (int) (Math.random() * width);
+        positionY = 10 + (int) (Math.random() * height);
         this.setBounds(positionX, positionY, width, height);
         this.addMouseListener(this);
         setFocusable(true);
